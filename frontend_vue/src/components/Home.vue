0de35e6dbbacdc330966bc26d959ae72d8138fa9
danamestrial <chayapolbun@gmail.com>
2021-08-24 20:12:59 +0700

    something to do with ;

--- frontend_vue/src/components/Home.vue
+++ frontend_vue/src/components/Home.vue
@@ -53,7 +53,7 @@ h2 {
           max-width="300"
           color="white"
         >
-          <h2 class="center" style="font-size: 40px;">Rate my Plan</h2>
+          <h2 class="center" style="font-size: 40px">Rate my Plan</h2>
         </v-card>
       </v-col>
     </v-row>
