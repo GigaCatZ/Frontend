f692042d4d2e26bf5a03e06f6c1167d1b709c6f9
Vanessa5331 <earth5331@gmail.com>
2021-08-27 00:07:51 +0700

    Add likes and 'continue the thread' buttons

--- frontend_vue/src/components/Home.vue
+++ frontend_vue/src/components/Home.vue
@@ -12,10 +12,10 @@
           elevation="2"
           height="280px"
         >
-          <v-carousel-item v-for="item in announcements" :key="item">
+          <v-carousel-item v-for="a in announcements" :key="a.title">
             <v-card outlined height="280px">
-              <v-img :src="item.src" aspect-ratio="16/9" height="200"></v-img>
-              <v-card-title v-text="item.title"></v-card-title>
+              <v-img :src="a.src" aspect-ratio="16/9" height="200"></v-img>
+              <v-card-title v-text="a.title"></v-card-title>
             </v-card>
           </v-carousel-item>
         </v-carousel>
@@ -50,9 +50,9 @@
       <v-card-title class="text-h4">Popular Tags</v-card-title>
       <div class="text-center">
         <v-chip
-          v-for="item in popularTags"
-          :key="item"
-          v-text="item"
+          v-for="tag in popularTags"
+          :key="tag"
+          v-text="tag"
           class="ma-2"
         ></v-chip>
       </div>
@@ -73,18 +73,27 @@
               single-line
               outlined
               full-width
-              any
             ></v-select>
           </v-col>
           <v-col><h2>Threads</h2></v-col>
         </v-row>
 
-        <v-row align="center" v-for="item in threads" :key="item">
+        <v-row align="center" v-for="t in threads" :key="t.title">
           <v-col>
             <v-card outlined>
-              <v-card-subtitle v-text="item.user"></v-card-subtitle>
-              <v-card-title class="text-h3" v-text="item.title"></v-card-title>
-              <v-card-text v-text="item.text"></v-card-text>
+              <v-card-subtitle v-text="t.user"></v-card-subtitle>
+              <v-card-title class="text-h3" v-text="t.title"></v-card-title>
+              <v-card-text v-text="t.text"></v-card-text>
+              <v-card-actions>
+                <v-btn class="ml-2 mt-3" text>
+                  <v-icon class="mr-1" color="grey">mdi-thumb-up</v-icon>
+                  <span v-text="t.likes"></span>
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn class="ml-2 mt-3" text>
+                  Continue the thread<v-icon left> mdi-arrow-right </v-icon>
+                </v-btn>
+              </v-card-actions>
             </v-card>
           </v-col>
         </v-row>
@@ -125,11 +134,36 @@ export default {
       ordering: ["RECENT", "POPULAR"],
       order: "RECENT",
       threads: [
-        { title: "Title 1", text: "example text", user: "username" },
-        { title: "Title 2", text: "example text", user: "username" },
-        { title: "Title 3", text: "example text", user: "username" },
-        { title: "Title 4", text: "example text", user: "username" },
-        { title: "Title 5", text: "example text", user: "username" },
+        {
+          title: "Title 1",
+          text: "example text",
+          user: "username",
+          likes: "10",
+        },
+        {
+          title: "Title 2",
+          text: "example text",
+          user: "username",
+          likes: "10",
+        },
+        {
+          title: "Title 3",
+          text: "example text",
+          user: "username",
+          likes: "10",
+        },
+        {
+          title: "Title 4",
+          text: "example text",
+          user: "username",
+          likes: "10",
+        },
+        {
+          title: "Title 5",
+          text: "example text",
+          user: "username",
+          likes: "10",
+        },
       ],
     };
   },
