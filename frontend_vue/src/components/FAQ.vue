5c13bfd089ace1f90e36c8da15ebe2f788d50395
nawat <67263644+justnawat@users.noreply.github.com>
2021-08-26 10:54:18 +0700

    FAQ question2

--- frontend_vue/src/components/FAQ.vue
+++ frontend_vue/src/components/FAQ.vue
@@ -1,5 +1,5 @@
 <style scoped>
-hr.solid {
+hr.title-solid {
   border-top: 2px solid #909090;
 }
 span.smaller {
@@ -14,45 +14,53 @@ span.smaller {
     <div class="text-left">
       <h1 class="font-weight-bold">
         Frequently Asked Questions
-        <span class="smaller">
-          by moderators and Ajarns
-        </span>
+        <span class="smaller">by moderators and Ajarns</span>
       </h1>
-      <hr class="solid">
+      <hr class="title-solid" />
       <!-- question 1 -->
+      <br />
+      <h2 class="font-weight-bold">
+        Can class X be open in the upcoming trimester?
+      </h2>
+      <p>
+        Depends. MUIC requires that a course has 7 people for a section to be
+        opened. If the course has low demand (i.e. Computer Architecture), you'd
+        need to have at least 6 other people interested in taking the class.
+      </p>
+      <p>
+        Also, there are some difficulties (good ones, according to Aj.Kritya) in
+        many faculty member's schedules. So, it could be quite difficult to have
+        an Ajarn teach multiple courses in a single trimester due to their other
+        commitments.
+      </p>
+      <br />
+
       <div>
-        <br/>
-        <h2 class="font-weight-bold">
-          Can class X be open in the upcoming trimester?
-        </h2>
+        <h2>Where do we register for our classes?</h2>
         <p>
-          Depends. MUIC requires that a course has 7 people for a section to be opened.
-          If the course has low demand (i.e. Computer Architecture), you'd need to have at least 6 other people
-          interested in taking the class.
-        </p>
-        <p>
-          Also, there are some difficulties (good ones, according to Aj.Kritya) in many faculty member's schedules.
-          So, it could be quite difficult to have an Ajarn teach multiple courses in a single trimester due to
-          their other commitments.
+          For Trimester 1 of Academic Year 2021-2022, course registration,
+          tuition payment, and add/drop will take place on
+          <a
+            href="http://sky.muic.mahidol.ac.th/?data%5Btype%5D%5B%5D=login"
+            target="_blank"
+            >SKY
+          </a>
+          However, we will soon be switching over to SKY+ in Trimester 2.
         </p>
       </div>
     </div>
 
     <!-- FAQ by dupes -->
-    <br/>
+    <br />
     <div class="text-left">
       <h1 class="font-weight-bold">
         Frequently Asked Questions
-        <span class="smaller">
-          by number of duplicates
-        </span>
+        <span class="smaller"> by number of duplicates </span>
       </h1>
-      <hr class="solid">
+      <hr class="title-solid" />
     </div>
   </v-container>
 </template>
-
-
 
 <script>
 export default {
