706275aa34849117e4fe4605a4f8a4fee5f1834b
nawat <67263644+justnawat@users.noreply.github.com>
2021-09-03 01:02:46 +0700

    Forgot password component added and awaiting backend support

--- frontend_vue/src/components/Login.vue
+++ frontend_vue/src/components/Login.vue
@@ -8,67 +8,73 @@
     <br />
     <!--login form-->
     <v-row>
-      <v-col offset="3" cols="12" sm="6" md="6" align-self="center">
+      <v-col align-self="center" cols="12" md="6" offset="3" sm="6">
         <v-form ref="form" v-model="valid" lazy-validation>
-          <v-alert type="error" v-model="login_alert" dismissible>
-            {{ login_alert_text }}</v-alert
-          >
-          <v-alert type="success" v-model="register_alert" dismissible>
-            {{ register_alert_text }}</v-alert
-          >
+          <v-alert v-model="login_alert" dismissible type="error">
+            {{ login_alert_text }}
+          </v-alert>
+          <v-alert v-model="register_alert" dismissible type="success">
+            {{ register_alert_text }}
+          </v-alert>
+
           <!--username field-->
           <v-text-field
-            :rules="usernameRules"
             v-model="username"
-            label="Enter Username"
-            filled
+            :rules="usernameRules"
             clearable
+            filled
+            label="Enter Username"
             @keydown.enter="login"
           ></v-text-field>
           <v-spacer></v-spacer>
+
           <!--password field-->
           <v-text-field
-            :append-icon="show3 ? 'mdi-eye' : 'mdi-eye-off'"
-            :type="show3 ? 'text' : 'password'"
-            :rules="passwordRules"
             v-model="password"
-            label="Enter Password"
-            filled
+            :append-icon="show3 ? 'mdi-eye' : 'mdi-eye-off'"
+            :rules="passwordRules"
+            :type="show3 ? 'text' : 'password'"
             clearable
+            filled
+            label="Enter Password"
             @keydown.enter="login"
             @click:append="show3 = !show3"
           ></v-text-field>
           <v-checkbox
-            color="#1a0086"
             v-model="remember"
+            color="#1a0086"
             label="Stay Signed In"
           ></v-checkbox>
         </v-form>
         <br />
-        <v-row cols="12" sm="6" md="4" justify="space-around">
+        <v-row cols="12" justify="space-around" md="4" sm="6">
           <!--login button-->
           <v-btn
-            :loading="loading"
             :disabled="!valid"
+            :loading="loading"
             color="#2a0094"
             dark
             elevation="5"
             @click="login"
-            >Login</v-btn
-          >
+            >Login
+          </v-btn>
+
+          <!-- forgot password button -->
+          <ForgotPwd />
+
           <!--register button w/ form-->
           <v-dialog v-model="form" max-width="600px">
             <template v-slot:activator="{ on, attrs }">
-              <v-btn color="#2a0094" dark v-bind="attrs" v-on="on">
+              <v-btn v-bind="attrs" v-on="on" color="#2a0094" dark>
                 Register
               </v-btn>
             </template>
             <v-spacer></v-spacer>
             <v-card>
               <div>
-                <v-alert type="error" v-model="register_alert" dismissible>
-                  {{ register_alert_text }}</v-alert
-                >
+                <v-alert v-model="register_alert" dismissible type="error">
+                  {{ register_alert_text }}
+                </v-alert>
               </div>
               <v-card-title>
                 <span class="text-h5">Register</span>
@@ -92,16 +98,16 @@
 
                     <v-text-field
                       v-model="studentID"
-                      label="Sky Username"
                       :rules="usernameRules"
+                      label="Sky Username"
                       required
                     ></v-text-field>
 
                     <v-text-field
                       v-model="password2"
                       :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                      :type="show1 ? 'text' : 'password'"
                       :rules="passwordRules"
+                      :type="show1 ? 'text' : 'password'"
                       label="Password"
                       required
                       @click:append="show1 = !show1"
@@ -110,8 +116,8 @@
                     <v-text-field
                       v-model="passwordConfirm"
                       :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
-                      :type="show2 ? 'text' : 'password'"
                       :rules="passwordRules.concat(passwordConfirmationRule)"
+                      :type="show2 ? 'text' : 'password'"
                       label="Confirm password"
                       required
                       @click:append="show2 = !show2"
@@ -119,8 +125,8 @@
 
                     <v-text-field
                       v-model="email"
-                      label="Email"
                       :rules="emailRules"
+                      label="Email"
                       required
                     ></v-text-field>
                   </v-form>
@@ -133,9 +139,9 @@
                 </v-btn>
                 <v-btn
                   color="blue darken-1"
+                  method="post"
                   text
                   @click="createuser"
-                  method="post"
                 >
                   Save
                 </v-btn>
@@ -153,10 +159,15 @@ import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
 // import { mdiCheckAll } from "@mdi/js";
+import ForgotPwd from "./ForgotPwd";
 
 Vue.use(VueAxios, axios);
 export default {
   name: "Login",
+
+  components: {
+    ForgotPwd,
+  },
 
   data: () => ({
     //Login Data//
